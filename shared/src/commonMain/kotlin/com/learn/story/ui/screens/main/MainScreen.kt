f3e9dc60f633339d1df641abf72720a52c914ad1
Wira Respati <wirarespatibppn@gmail.com>
2026-09-08 11:59:36 +0800

    Clean up unused imports in MainScreen

--- shared/src/commonMain/kotlin/com/learn/story/ui/screens/main/MainScreen.kt
+++ shared/src/commonMain/kotlin/com/learn/story/ui/screens/main/MainScreen.kt
@@ -1,11 +1,9 @@
 package com.learn.story.ui.screens.main
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Bookmark
@@ -24,7 +22,6 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -32,12 +29,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.learn.story.ui.screens.home.HomeViewModel
 import com.learn.story.ui.screens.home.StoryListScreen
 import com.learn.story.ui.screens.map.StoryMapScreen
